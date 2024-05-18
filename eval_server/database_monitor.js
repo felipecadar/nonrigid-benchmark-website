e8b14eb7ff65b03fb15b1fd45fa5d2dab325ba22
Felipe Cadar <cadar@dcc.ufmg.br>
2024-05-18 22:21:36 +0200

    test

--- eval_server/database_monitor.js
+++ eval_server/database_monitor.js
@@ -45,14 +45,14 @@ async function eval_loop() {
         const outPath = path + '.out'
 
         // process file
-        const python = spawn.spawn('python', [
+        const python = spawn('python', [
           '-m nonrigid_benchmark.evaluate',
           `--input ${path}`,
           `--output ${outPath}`,
           `--dataset ${datasetPath}`,
           `--split ${experiment.split}`,
         ])
-        
+
         python.stdout.on('data', (data) => {
             console.log(`stdout: ${data}`)
         })
