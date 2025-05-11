9ccfc060c551e318ea82ccdfcad38860dcc5feb0
Felipe Cadar <cadar@dcc.ufmg.br>
2025-05-11 16:19:50 -0300

    update eval server

--- eval_server/database_monitor.js
+++ eval_server/database_monitor.js
@@ -1,199 +1,235 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
+import fs from "fs";
+import spawn from "child_process";
+import fetch from "node-fetch";
 
-import { PrismaClient, Status } from '@prisma/client'
-import fs from 'fs'
-import spawn from 'child_process'
-import fetch from 'node-fetch'
+// const prisma = new PrismaClient();
 
+import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
 
-import { createClient } from '@supabase/supabase-js'
+const adapter = new PrismaLibSQL({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+});
+const prisma = new PrismaClient({ adapter });
 
-// Create a single supabase client for interacting with your database
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+const thisDir = fs.realpathSync(process.cwd());
 
-const prisma = new PrismaClient()
-const thisDir = fs.realpathSync(process.cwd())
+// https://upload.benchmark.eucadar.com/cmacqy3bx00008s6evii07wwg/SIFT_2048/Single%20Object/deformation_1-illumination-viewpoint.json
+const BASE_URL = "upload.benchmark.eucadar.com";
 
 async function eval_loop() {
-    const dir = process.env.EXPERIMENT_DIR //'/volumes/nonrigid_dataset/experiments'
-    const dataset_dir = process.env.DATASET_DIR //'/volumes/nonrigid_dataset/dataset'
+  const dir = process.env.EXPERIMENT_DIR; //'/volumes/nonrigid_dataset/experiments'
+  const dataset_dir = process.env.DATASET_DIR; //'/volumes/nonrigid_dataset/dataset'
 
-    const dataset_mapping = {
-        'Multiple Object': 'test_multiple_obj',
-        'Single Object': 'test_single_obj',
-        'Scale': 'test_scale',
-    }
+  const dataset_mapping = {
+    "Multiple Object": "test_multiple_obj",
+    "Single Object": "test_single_obj",
+    Scale: "test_scale",
+  };
 
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir)
-    }
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
 
-    // const all_not_processed = await prisma.experiment.findMany({
-    //     where: {
-    //         status: Status.PENDING
-    //     }
-    // })
+  // const all_not_processed = await prisma.experiment.findMany({
+  //     where: {
+  //         status: "PENDING"
+  //     }
+  // })
 
-    // get oldest experiment with status pending
-    const all_not_processed = await prisma.experiment.findMany({
-        where: {
-            status: {
-                in: [Status.PENDING, Status.REPROCESS]
-            }
-        },
-        orderBy: {
-            createdAt: 'asc'
-        },
-        take: 1
-    })
+  // get oldest experiment with status pending
+  const all_not_processed = await prisma.experiment.findMany({
+    where: {
+      status: {
+        in: ["PENDING", "REPROCESS"],
+      },
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+    take: 1,
+  });
 
-    if (all_not_processed.length === 0) {
-        console.log('No experiments to process')
-        return true
-    }
+  if (all_not_processed.length === 0) {
+    console.log("No experiments to process");
+    return true;
+  }
 
-    for (const experiment of all_not_processed) {
-        // download file experiment.matchFileURL 
-        const matchFileURL = experiment.matchFileURL
-        const path = `${dir}/${experiment.id}.json`
-        console.log(`Downloading ${matchFileURL} to ${path}`)
-         
-        const {data, error} = await supabase.storage.from('nonrigid-benchmark').createSignedUrl(matchFileURL, 60)
+  // console.log(all_not_processed);
+
+  for (const experiment of all_not_processed) {
+    // download file experiment.matchFileURL
+    const matchFileURL = encodeURI("https://" + BASE_URL + "/" + experiment.matchFileURL);
+    const path = `${dir}/${experiment.id}.json`;
+    const datasetPath = `${dataset_dir}/${dataset_mapping[experiment.dataset]}/`;
+
+    // download file
+    const response = await fetch(matchFileURL);
+    const buffer = await response.text();
+    let ended = false;
+
+    fs.writeFileSync(path, buffer);
+    console.log(`Downloaded ${matchFileURL} to ${path}`);
+
+    // process file
+    // docker build -t eval_server -f eval_server/Dockerfile .
+    // check if docker image exists, if not, build it
+    const imageName = "eval_server";
+    const imageExists = await new Promise((resolve) => {
+      spawn.exec(`docker images -q ${imageName}`, (error, stdout, stderr) => {
         if (error) {
-            console.error(error)
-
-            // set to failed
-            await prisma.experiment.update({
-                where: {
-                    id: experiment.id
-                },
-                data: {
-                    status: Status.FAILED
-                }
-            })
-            return false
+          console.error(`exec error: ${error}`);
+          resolve(false);
         }
-        const real_url = data.signedUrl
-
-        const datasetPath = `${dataset_dir}/${dataset_mapping[experiment.dataset]}/`
-
-        // download file
-        const response = await fetch(real_url)
-        const buffer = await response.text()
-        let ended = false
-
-        fs.writeFileSync(path, buffer)
-        console.log(`Downloaded ${matchFileURL} to ${path}`)
-        const outPath = path + '.out'
-
-        // process file
-        const python = spawn.spawn('eval_server/run_python.sh', [
-        //   '-m nonrigid_benchmark.evaluate',
-          `--input ${path}`,
-          `--output ${outPath}`,
-          `--dataset ${datasetPath}`,
-          `--split ${experiment.split}`,
-          `--nproc 4`,
-        ])
-        
-        python.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`)
-        })
-
-        python.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`)
-        })
-
-        python.on('error', async (error) => {
-            console.error(`error: ${error.message}`)
-            ended = true
-            await prisma.experiment.update({
-                where: {
-                    id: experiment.id
-                },
-                data: {
-                    status: Status.FAILED
-                }
-            })
+        if (stdout) {
+          resolve(true);
+        } else {
+          resolve(false);
         }
-        )
-        
-        // on start
-        python.on('spawn', async () => {
-          await prisma.experiment.update({
-              where: {
-                  id: experiment.id
-              },
-              data: {
-                  status: Status.PROCESSING
-              }
-          })        
-        })
-
-        python.on('close', async (code) => {
-            console.log(`child process exited with code ${code}`)
-            ended = true
-
-            if (code === 0) {
-                // read file + .out 
-                const outPath = path + '.out'
-                const out = fs.readFileSync(outPath, 'utf8')
-                // parse ms,ma,mr
-                const [ms, ma, mr] = out.split(',')
-                // update experiment
-
-                // update status
-                await prisma.experiment.update({
-                    where: {
-                        id: experiment.id
-                    },
-                    data: {
-                        status: Status.COMPLETED,
-                        ms: parseFloat(ms),
-                        ma: parseFloat(ma),
-                        mr: parseFloat(mr),
-                    }
-                })
-                console.log(`ms: ${ms}, ma: ${ma}, mr: ${mr}`)
-            } else {
-                // update status
-                await prisma.experiment.update({
-                    where: {
-                        id: experiment.id
-                    },
-                    data: {
-                        status: Status.FAILED
-                    }
-                })
-                console.error(`Error processing experiment ${experiment.id}`)
-            }
-
-        })
-
-        while (!ended) {
-            await new Promise(r => setTimeout(r, 1000))
-        }
-
+      }
+      );
+    });
+    if (!imageExists) {
+      console.log(`Docker image ${imageName} does not exist, building...`);
+      await new Promise((resolve, reject) => {
+        spawn.exec(`docker build -t ${imageName} -f ${thisDir}/eval_server/Dockerfile ${thisDir}/eval_server`, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
+            reject(error);
+          }
+          console.log(`stdout: ${stdout}`);
+          console.error(`stderr: ${stderr}`);
+          resolve(true);
+        });
+      }
+      );
+    } else {
+      console.log(`Docker image ${imageName} exists, skipping build...`);
     }
-    return true
+
+    const python = spawn.spawn("docker", [
+      "run",
+      "--rm",
+      "-v", `${dataset_dir}:/app/nonrigid_dataset`,
+      "-v", `${dir}:/app/experiments/`,
+      "eval_server",
+      "python3", "-m", "nonrigid_benchmark.evaluate",
+      `--input`, `/app/experiments/${experiment.id}.json`,
+      `--output`, `/app/experiments/${experiment.id}.out`,
+      `--dataset`, `/app/nonrigid_dataset/${dataset_mapping[experiment.dataset]}/`,
+      `--split`, `${experiment.split}`,
+      `--nproc`, "10",
+    ]);
+
+    python.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    python.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    python.on("error", async (error) => {
+      console.error(`error: ${error.message}`);
+      ended = true;
+      await prisma.experiment.update({
+        where: {
+          id: experiment.id,
+        },
+        data: {
+          status: "FAILED",
+        },
+      });
+    });
+
+    // on start
+    python.on("spawn", async () => {
+      await prisma.experiment.update({
+        where: {
+          id: experiment.id,
+        },
+        data: {
+          status: "PROCESSING",
+        },
+      });
+    });
+
+    python.on("close", async (code) => {
+      console.log(`child process exited with code ${code}`);
+      ended = true;
+
+      if (code === 0) {
+        // wait one second
+        await new Promise((r) => setTimeout(r, 1000));
+        // check if file exists
+        const outPath = `${dir}/${experiment.id}.out`;
+        if (!fs.existsSync(outPath)) {
+          console.error(`Output file ${outPath} does not exist`);
+          // update status
+          await prisma.experiment.update({
+            where: {
+              id: experiment.id,
+            },
+            data: {
+              status: "FAILED",
+            },
+          });
+          return;
+        }
+        // read file + .out
+        const out = fs.readFileSync(outPath, "utf8");
+        // parse ms,ma,mr
+        const [ms, ma, mr] = out.split(",");
+        // update status
+        await prisma.experiment.update({
+          where: {
+            id: experiment.id,
+          },
+          data: {
+            status: "COMPLETED",
+            ms: parseFloat(ms),
+            ma: parseFloat(ma),
+            mr: parseFloat(mr),
+          },
+        });
+        console.log(`ms: ${ms}, ma: ${ma}, mr: ${mr}`);
+      } else {
+        // update status
+        await prisma.experiment.update({
+          where: {
+            id: experiment.id,
+          },
+          data: {
+            status: "FAILED",
+          },
+        });
+        console.error(`Error processing experiment ${experiment.id}`);
+      }
+    });
+
+    while (!ended) {
+      await new Promise((r) => setTimeout(r, 1000));
+    }
+  }
+  return true;
 }
 
 async function main() {
   while (true) {
-    const r = await eval_loop()
+    const r = await eval_loop();
     if (r) {
-        await new Promise(r => setTimeout(r, 10000))
+      await new Promise((r) => setTimeout(r, 10000));
     }
   }
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- eval_server/run_python.sh
+++
@@ -1,2 +0,0 @@
-# call with all arguments you want to pass to the python script
-$HOME/miniconda3/envs/nonrigid_benchmark/bin/python -m nonrigid_benchmark.evaluate $@ 
